41eee8f6ae95e32131ab111e506f3617bf5f9263
Dmels <136374991+Edwinale20@users.noreply.github.com>
2026-02-08 18:36:37 -0600

    Change category input to a select box

--- pages/🧮 Ingresar Datos.py
+++ pages/🧮 Ingresar Datos.py
@@ -109,7 +109,7 @@ def append_row_to_onedrive_excel(access_token, file_id, sheet_name, row_dict):
 # Inputs
 Nombre = st.text_input("🖋️ Ingresa la Descripción:")
 Cantidad = st.number_input("💲Ingresa el monto:")
-Categoria = st.text_input("🍻 Ingresa la categoría:")
+Categoria = st.selectbox("📌 Tipo", ["Gasto", "Ingreso", "PagoDeuda"])
 fecha = st.date_input("🗓️ Selecciona la fecha:")
 
 Submit = st.button("Ingresar")
